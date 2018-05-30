1f6df28c31a008341759fbaa2e1e1896de29f422
xy2z <xyzbk89@gmail.com>
2018-05-30 20:53:47 +0200

    Fixed feed title and body html

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
   <channel>
-    <title><![CDATA[Feed]]></title>
+    <title><![CDATA[{{ env('APP_NAME') }}]]></title>
     <description><![CDATA[]]></description>
     <link><![CDATA[{{ URL::route('home') }}]]></link>
     <language><![CDATA[en]]></language>
@@ -10,7 +10,7 @@
         <item>
             <guid isPermaLink="false"><![CDATA[{{ URL::to('/') }}/news/{{ $row->id }}]]></guid>
             <title><![CDATA[{{ $row->title }}]]></title>
-            <description><![CDATA[{{ $row->body }}]]></description>
+            <description><![CDATA[{!! $row->body !!}]]></description>
             <link><![CDATA[{{ URL::to('/') }}/news/{{ $row->id }}]]></link>
 
             <pubDate><![CDATA[{{ date(DATE_RFC2822, strtotime($row->created_at)) }}]]></pubDate>
